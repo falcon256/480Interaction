86a0cea5da1d529780d838b28bbb9b74c7c85537
256 Games <256games@gmail.com>
2020-01-31 14:42:05 -0700

    Started on scaling and scripting the new UI stuff.

---
+++ Assets/Scripts/SwitchController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+    private float switchStartingAngle = -30.0f;
+    private float switchOnAngle = 30.0f;
+    private float switchOffAngle = -30.0f;
+    private float angleDeadzone = 1.0f;
+    public GameObject switchBase = null;
+    public GameObject switchLever = null;
+    public GameObject switchGuard = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(switchLever==null)
+        {
+            Debug.LogError("You didn't set up a switch correctly.");
+            return;
+        }
+
+        switchLever.transform.localRotation = Quaternion.Euler(0,0, switchStartingAngle);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(1, 100) > 98)
+            Debug.Log(switchLever.transform.localRotation.eulerAngles.z);
+    }
+}
