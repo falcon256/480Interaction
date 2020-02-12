a2a9324decc1c5d5899e6c4d5eb50302a910ed75
256 Games <256games@gmail.com>
2020-02-11 17:26:39 -0700

    Just a push, stepping away

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ButtonController : MonoBehaviour
+{
+    public TextMeshPro buttonText;
+    public Color textOffColor = Color.black;
+    public Color textOnColor = Color.white;
+    public bool isOn = true;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
