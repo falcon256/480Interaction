cdfdbae14d70a37699d190f13c848054312b5784
256 Games <256games@gmail.com>
2020-03-03 16:19:34 -0700

    Added output display to sliders

--- Assets/Scripts/SliderController.cs
+++ Assets/Scripts/SliderController.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 
 public class SliderController : MonoBehaviour
 {
 
     public GameObject handle = null;
     public float output = 0;
-    public bool takeControlNextFrame = false;
-
+    public TextMeshPro outputTMP = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,20 +24,16 @@ public class SliderController : MonoBehaviour
         handlePos.x = 0;
         handlePos.y = 0;
         handlePos.z = Mathf.Clamp(handlePos.z, -25.0f, 25.0f);
-        output = handlePos.z + 25.0f;
+        output = (handlePos.z + 25.0f)*2.0f;
         handle.GetComponent<Rigidbody>().transform.localPosition = handlePos;
-        if (takeControlNextFrame)
-            if (handle.GetComponent<PhotonView>().Owner != PhotonNetwork.LocalPlayer)
-            {
-                handle.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.LocalPlayer);
-                takeControlNextFrame = false;
-            }
+        outputTMP.text = string.Format("{0:00.0}", output);
     }
 
+    /* our other code makes this not needed.
     private void OnCollisionEnter(Collision collision)
     {
         if(handle.GetComponent<PhotonView>().Owner!=PhotonNetwork.LocalPlayer)
             handle.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.LocalPlayer);
     }
-
+    */
 }
---
+++ Assets/Scripts/SliderHandleController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class SliderHandleController : MonoBehaviourPun
+{
+    public SliderController parentSlider = null;
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void updatePositionOnPeers()
+    {
+        PhotonView pv = this.photonView;
+        pv.RPC("handlePositionUpdate", RpcTarget.Others, (object)this.GetComponent<Rigidbody>().transform);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        updatePositionOnPeers();
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        updatePositionOnPeers();
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        updatePositionOnPeers();
+    }
+
+    [PunRPC]
+    public void handlePositionUpdate(Transform trans)
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+        rb.transform.localPosition = trans.localPosition;
+        rb.transform.localRotation = trans.localRotation;
+        rb.transform.position = trans.position;
+        rb.transform.rotation = trans.rotation;
+    }
+
+}
